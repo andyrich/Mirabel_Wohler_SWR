669971112fb9184629c2af347afa5d9069b7a0eb
arich <arich@scwa.ca.gov>
2023-01-05 18:11:20 -0800

    changed

---
+++ pest/run_pest.bat
@@ -0,0 +1,4 @@
+rem cd ..
+rem pestpp-opt.exe reg_pest.pst /H :4004 /j
+start powershell.exe -ExecutionPolicy bypass
+pest++.exe reg_pest.pst 
---
+++ pest/run_pest_pp_slave.bat
@@ -0,0 +1,3 @@
+rem pest++ lith_v7.pst  /h 216.121.74.34:4004 
+rem pest++  /h 10.10.1.4:4004 
+pest++ reg_pest.pst /h 192.168.0.219:4004
