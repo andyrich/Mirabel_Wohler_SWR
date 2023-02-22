c5422e08b0d45b9c986ad49a24ca26d5dc758376
arich <arich@scwa.ca.gov>
2023-02-22 13:29:38 -0800

    init

---
+++ pest/run_pest_once.bat
@@ -0,0 +1,6 @@
+rem cd ..
+rem pestpp-opt.exe reg_pest.pst /H :4004 /j
+start powershell.exe -ExecutionPolicy bypass
+call C:\Users\arich\AppData\Local\mambaforge\condabin\conda_hook.bat
+call conda activate gis_py_8
+pest++.exe reg_pest.pst 
